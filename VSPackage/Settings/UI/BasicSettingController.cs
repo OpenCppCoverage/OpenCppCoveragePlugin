1fe63830eec5f4a48be44181c1c858e0eec94dd7
OpenCppCoverage <OpenCppCoverage@gmail.com>
2016-09-08 09:37:32 +0200

    Remove BasicSettingControllerTests and do some minor cleanings.

--- VSPackage/Settings/UI/BasicSettingController.cs
+++ VSPackage/Settings/UI/BasicSettingController.cs
@@ -36,7 +36,7 @@ namespace OpenCppCoverage.VSPackage.Settings.UI
     }
 
     //-------------------------------------------------------------------------
-    class BasicSettingController: PropertyChangedNotifier
+    class BasicSettingController
     {
         //---------------------------------------------------------------------
         public BasicSettingController()
