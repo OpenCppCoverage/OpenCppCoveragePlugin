ea84eb30468301ba8fa2b7740f246c9c7806de4d
OpenCppCoverage <OpenCppCoverage@gmail.com>
2017-06-09 09:43:01 +0200

    Fix CheckCoverage.CoverageTreeTests.

--- VSPackage_IntegrationTests/IntegrationTestsSolution/CppConsoleApplication/CppConsoleApplication.cpp
+++ VSPackage_IntegrationTests/IntegrationTestsSolution/CppConsoleApplication/CppConsoleApplication.cpp
@@ -8,13 +8,13 @@
 int _tmain(int argc, _TCHAR* argv[])
 {																	// COVERED
 	if (argc == 2 && argv[1] == std::wstring(L"TestEnvVariable"))	// COVERED
-	{																// UNCOVERED
+	{
 		char* value = nullptr;										// UNCOVERED
 		size_t size = 0;											// UNCOVERED
 		_dupenv_s(&value, &size, "EXIT_CODE");						// UNCOVERED
 		if (value)													// UNCOVERED
 			return std::atoi(value);								// UNCOVERED
-	}																// UNCOVERED
+	}
 	if (false)														// COVERED
 		Hello();													// UNCOVERED
 	else															// UNCOVERED
