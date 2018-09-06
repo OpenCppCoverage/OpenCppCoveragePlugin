f9cdd4470ac275f25b3b25d3de5f081235fe3a9e
OpenCppCoverage <OpenCppCoverage@gmail.com>
2018-09-06 14:20:27 +0200

    Use Is64BitOperatingSystem to select OpenCppCoverage architecture.

--- VSPackage/OpenCppCoverage.cs
+++ VSPackage/OpenCppCoverage.cs
@@ -72,50 +72,9 @@ namespace OpenCppCoverage.VSPackage
         {
             var assemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
             var assemblyFolder = Path.GetDirectoryName(assemblyLocation);
-            var openCppCovergeFolder = 
-                Is64bitsExecutable(commandPath) ? 
-                    "OpenCppCoverage-x64" : "OpenCppCoverage-x86";
-
+            var openCppCovergeFolder = Environment.Is64BitOperatingSystem ? 
+                                            "OpenCppCoverage-x64" : "OpenCppCoverage-x86";
             return Path.Combine(assemblyFolder, openCppCovergeFolder, "OpenCppCoverage.exe");
-        }
-
-        //---------------------------------------------------------------------
-        bool Is64bitsExecutable(string commandPath)
-        {
-            NativeMethods.BinaryType binaryType;
-
-            if (NativeMethods.GetBinaryType(commandPath, out binaryType) == 0)
-                throw new VSPackageException("Invalid executable: " + commandPath);
-
-            outputWindowWriter.WriteLine(
-                string.Format("{0} has binary type: {1}", commandPath, binaryType.ToString()));
-
-            if (binaryType != NativeMethods.BinaryType.SCS_64BIT_BINARY 
-                && binaryType != NativeMethods.BinaryType.SCS_32BIT_BINARY)
-            {
-                throw new VSPackageException(
-                    string.Format("Invalid binary format {0} for {1}", binaryType.ToString(), commandPath));
-            }
-
-            return binaryType == NativeMethods.BinaryType.SCS_64BIT_BINARY;
-        }
-
-        //---------------------------------------------------------------------
-        class NativeMethods
-        {
-            [DllImport("Kernel32.dll", CharSet = CharSet.Unicode)]
-            public extern static Int32 GetBinaryType(string lpApplicationName, out BinaryType lpBinaryType);
-
-            public enum BinaryType : uint
-            {
-                SCS_32BIT_BINARY = 0, // A 32-bit Windows-based application
-                SCS_64BIT_BINARY = 6, // A 64-bit Windows-based application.
-                SCS_DOS_BINARY = 1, // An MS-DOS – based application
-                SCS_OS216_BINARY = 5, // A 16-bit OS/2-based application
-                SCS_PIF_BINARY = 3, // A PIF file that executes an MS-DOS – based application
-                SCS_POSIX_BINARY = 4, // A POSIX – based application
-                SCS_WOW_BINARY = 2 // A 16-bit Windows-based application
-            }
         }
     }
 }
