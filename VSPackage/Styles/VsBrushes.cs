f35acc344a5192279cff12c8f90f87e151d7749c
OpenCppCoverage <OpenCppCoverage@gmail.com>
2018-09-20 14:57:56 +0200

    Do not use Microsoft.VisualStudio.Shell.15.0 in XAML.

---
+++ VSPackage/Styles/VsBrushes.cs
@@ -0,0 +1,26 @@
+﻿using ShellVsBrushes = Microsoft.VisualStudio.Shell.VsBrushes;
+
+namespace OpenCppCoverage.VSPackage.Styles
+{
+    class VsBrushes
+    {
+        public static object CaptionTextKey => ShellVsBrushes.CaptionTextKey;
+        public static object ToolWindowBackgroundKey => ShellVsBrushes.ToolWindowBackgroundKey;
+        public static object ButtonFaceKey => ShellVsBrushes.ButtonFaceKey;
+        public static object GridLineKey => ShellVsBrushes.GridLineKey;
+        public static object GridHeadingTextKey => ShellVsBrushes.GridHeadingTextKey;
+        public static object GridHeadingBackgroundKey => ShellVsBrushes.GridHeadingBackgroundKey;
+        public static object ButtonHighlightKey => ShellVsBrushes.ButtonHighlightKey;
+        public static object InactiveBorderKey => ShellVsBrushes.InactiveBorderKey;
+        public static object InactiveCaptionTextKey => ShellVsBrushes.InactiveCaptionTextKey;
+        public static object ComboBoxBackgroundKey => ShellVsBrushes.ComboBoxBackgroundKey;
+        public static object ComboBoxBorderKey => ShellVsBrushes.ComboBoxBorderKey;
+        public static object ToolWindowTabTextKey => ShellVsBrushes.ToolWindowTabTextKey;
+        public static object ToolWindowTabGradientBeginKey => ShellVsBrushes.ToolWindowTabGradientBeginKey;
+        public static object ToolWindowTabMouseOverBackgroundBeginKey => ShellVsBrushes.ToolWindowTabMouseOverBackgroundBeginKey;
+        public static object ToolWindowTabMouseOverBorderKey => ShellVsBrushes.ToolWindowTabMouseOverBorderKey;
+        public static object ToolWindowTabSelectedTextKey => ShellVsBrushes.ToolWindowTabSelectedTextKey;
+        public static object ToolWindowTabSelectedTabKey => ShellVsBrushes.ToolWindowTabSelectedTabKey;
+        public static object ButtonTextKey => ShellVsBrushes.ButtonTextKey;
+    }
+}
---
+++ VSPackage/Styles/VsColors.cs
@@ -0,0 +1,14 @@
+﻿using ShellVsColors = Microsoft.VisualStudio.Shell.VsColors;
+
+namespace OpenCppCoverage.VSPackage.Styles
+{
+    class VsColors
+    {
+        public static object InactiveBorderKey => ShellVsColors.InactiveBorderKey;
+        public static object ComboBoxBackgroundKey => ShellVsColors.ComboBoxBackgroundKey;
+        public static object ComboBoxMouseOverBackgroundBeginKey => ShellVsColors.ComboBoxMouseOverBackgroundBeginKey;
+        public static object ComboBoxMouseOverBackgroundEndKey => ShellVsColors.ComboBoxMouseOverBackgroundEndKey;
+        public static object ComboBoxBorderKey => ShellVsColors.ComboBoxBorderKey;
+        public static object ComboBoxMouseOverBorderKey => ShellVsColors.ComboBoxMouseOverBorderKey;
+    }
+}
